e9147df7b36c5e64147f546ce2759850e93171cb
coco <coco.wapplab@gmail.com>
2021-09-29 10:36:09 +0900

    스크립트 완료

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 const todoBtn = document.querySelector('.plus-btn'),
-    todoList = document.querySelector('.todo-list');
+    todoList = document.querySelector('.todo-list'),
+    tasks= document.querySelector('.num');
     
     const todos_ = 'todos'
     let todos = [];
-    let newID = todos.length + 1;
+    let local = JSON.parse(localStorage.getItem(todos_))
+    let newID = local === null ? todos.length : local[local.length - 1].id + 1
     
-function addTaskList(){
+//todo항목 추가 함수
+    function addTaskList(value){
     const todobox = document.createElement('div'),
     check = document.createElement('input'),
-    todo = document.createElement('div'),
-    delBtn = document.createElement('img');
+    delBtn = document.createElement('img'),
+    todo  = document.createElement('input')
 
     todobox.classList.add('todo-box');
     check.setAttribute('type','checkbox');
     todo.classList.add('todo');
-    todo.setAttribute('contenteditable','true')
+    // todo.setAttribute('contenteditable','true')
     delBtn.classList.add('delete-btn');
     delBtn.setAttribute('src', './assets/delete.svg')
     todo.id = newID
@@ -22,72 +25,130 @@ function addTaskList(){
     
     const text = todo.innerText;
     
-  
+  if(value === ''){
     todobox.append(check, todo, delBtn)
     todoList.append(todobox)
     todo.focus()
-    
+  }else{
+        todobox.append(check, todo, delBtn)
+        todoList.append(todobox)
+        todo.value = value.text
+        todo.id = value.id
+  }
    
 
-    delBtn.addEventListener('click', deleteTodo);
-    todo.addEventListener('focus', () => focusTodo(newID))
+    delBtn.addEventListener('click', () => deleteTodo(todo.id));
     todo.addEventListener('blur', () => inputBlurTodo(newID))
-    todo.addEventListener('click', () => inputClickTodo(todo.id))
     todo.addEventListener('keypress', () => keypressTodo(newID))
+    check.addEventListener('click', () => checkTodo())
 }
 
-function inputClickTodo(id){
-    newID = parseInt(id)
-
-    const text = event.target.innerText;
-    console.log(text);
-}
-
-
-function focusTodo(id){
-    if(event.blur){
-        inputBlurTodo(id)
-        }
-}
+//키프레스 엔터
 function keypressTodo(id){
     if(event.code === 'Enter'){
-    inputBlurTodo(id)
-    
+    event.target.blur()
     }
 }
+
+//엔터를 치거나 블러시 항목 추가
 function inputBlurTodo(id){
+    console.log(newID);
     const target = event.target
-    const text = target.innerText
-    event.target.blur()
     const todoObj = {
-        text,
+        text: target.value,
         id:newID
     }
-    const todoLen =  todos.filter(todo => todo.id === id).length;
+    const todoLen =  todos.filter(todo => {
+        return todo.id === parseInt(target.id)
+    }).length;
 
-   if(text !== '' && todoLen === 0){
+   if(target.value !== '' && todoLen === 0){
         todos.push(todoObj)
         newID++
         saveTodo()
     }
 
-    if(text === ''){
+    if(target.value === ''){
         target.parentNode.remove()
+        let todoLeng = todos.findIndex(todo => todo.id === parseInt(target.id))
+        if(todoLeng !== -1){
+           todos.splice(todoLeng, 1)
+           saveTodo()
+        }
+    }
+
+    //수정하기
+    const newItem = todos.filter(todo => todo.id === parseInt(target.id))[0]
+    if(newItem !== undefined){
+        if(newItem.text !== target.value){
+            todos.filter((todo) => {
+               if(todo.id === newItem.id){
+                todo.text = target.value
+                return todo;
+               }
+            });
+            saveTodo()
+        }
+    }
+}
+
+//로드시 로컬스토리지 todo 불러오기
+function onLoadTodo(){
+    const item = localStorage.getItem(todos_)
+    if(item !== null){
+        todos = JSON.parse(item)
+        for(let i=0; i < todos.length; i++){
+            addTaskList(todos[i])
+            // newID++
+        }
+    }
+}
+
+//task 개수
+function onTasksNum(){
+        tasks.innerText = todos.length
+}
+
+//로컬스토리지에 tood저장
+function saveTodo(){
+    localStorage.setItem(todos_, JSON.stringify(todos))
+    onTasksNum()
+}
+
+function deleteTodo(id){
+    target = event.target;
+    target.parentNode.remove()
+    
+    todos.filter((todo,index) => {
+        if(todo.id === parseInt(id)){
+            todos.splice(index, 1)
+            // todos.map((todo,index) => {
+            //     todo.id = index;
+            //     return todo
+            // })
+            saveTodo()
+        }
+    })
+
+}
+
+//체크박스 
+function checkTodo(){
+    const target = event.target;
+    const check = target.parentNode.childNodes[1]
+    if(target.checked){
+        check.classList.add('checked');
+        check.setAttribute('readonly', true)
+    }else{
+        check.removeAttribute('readonly')
     }
 }
 
 
-function saveTodo(){
-    localStorage.setItem(todos_, JSON.stringify(todos))
-}
-
-function deleteTodo(){
-    console.log('delete');
-}
-
-
 function init(){
-    todoBtn.addEventListener('click', addTaskList)
+    todoBtn.addEventListener('click', () => addTaskList(''))
+    onLoadTodo()
+    onTasksNum()
 }
 
 init()
